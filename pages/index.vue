add850a9d40d4f7c2893bd6bfe8fc6cafc362f99
Alexandre <alexandre.bueno0204@gmail.com>
2024-10-02 11:17:56 -0300

    landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,41 @@
 <template>
-  <div class="flex justify-center gap-4">
-    <Card class="min-w-[30%] max-w-[30%]">
-      <template #title> Unauthenticated API call </template>
+  <nav class="w-full bg-gray-800 py-0 fixed top-0 left-0">
+    <ul class="flex justify-center list-none p-0 m-0">
+      <li class="mx-4">
+        <router-link to="/register" class="text-white font-bold hover:underline">Login</router-link>
+      </li>
+      <li class="mx-4">
+        <router-link to="/" class="text-white font-bold hover:underline">NeetFinance</router-link>
+      </li>
+      <li class="mx-4">
+        <router-link to="/exercicios" class="text-white font-bold hover:underline">Exercícios</router-link>
+      </li>
+      <li class="mx-4">
+        <router-link to="/cursos" class="text-white font-bold hover:underline">Cursos</router-link>
+      </li>
+    </ul>
+  </nav>
 
-      <template #content>
-        <pre> {{ publicData }} </pre>
-      </template>
+  <div class="flex flex-col justify-center items-center mt-24"> <!-- mt 24 eh espacamento    margin top   mb margin  bottom-->
+    <h1 class="text-5xl font-bold mb-4">NeetFinance</h1> 
+    <h2 class="text-2xl font-bold leading-tight text-center"> <!--leadin eh espaco entre linhas e tight eh que eh menor q o normal -->
+      O melhor lugar para aprender finanças, <br /> do zero.
+    </h2>
+  </div>
 
-      <template #footer>
-        <Button @click="() => publicRefresh()">Fetch</Button>
-      </template>
-    </Card>
-    
-    <Card class="min-w-[30%] max-w-[30%]">
-      <template #title> Authenticated API call </template>
-
-      <template #content>
-        <pre class="text-nowrap overflow-clip"> {{ privateData }} </pre>
-      </template>
-
-      <template #footer>
-        <Button :disabled="status === 'unauthenticated'" @click="() => privateRefresh()">Fetch</Button>
-      </template>
-    </Card>
-
+  <!-- botao de versao tp get pro -->
+  <div class="flex justify-center mt-12"> 
+    <button class="border-2 border-sky-500 text-sky-500 bg-transparent font-bold text-[15px] py-4 px-6 rounded-md cursor-pointer hover:bg-sky-500 hover:text-white transition duration-500">
+      Get Pro
+    </button>
   </div>
 </template>
 
-<script lang="ts" setup>
-import type { User } from "next-auth";
-import type { Index } from "~/server/api/index.get";
-
-// This tells nuxt-auth that this page is public
-definePageMeta({ auth: false });
-
-const { status } = useAuth();
-
-const [
-	{ data: privateData, refresh: privateRefresh },
-	{ data: publicData, refresh: publicRefresh },
-] = await Promise.all([
-	useFetch<User>("/api/me", { server: false, immediate: false }),
-	useFetch<Index>("/api", { server: false, immediate: false }),
-]);
+<script>
 </script>
+
+<style>
+.my-cursor-pointer {
+  cursor: pointer;
+}
+</style>
